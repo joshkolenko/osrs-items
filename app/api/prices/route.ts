9950237ec477e726a6e284cb5512fc8caf5af0d2
Josh Kolenko <jkolenko+github@proton.me>
2024-02-17 23:59:03 -0500

    refactor: remove api/prices

--- app/api/prices/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from 'next/server';
-
-export async function GET(request: NextRequest) {
-  const res = await fetch(process.env.PRICES_API_URL);
-  const data = await res.json();
-
-  return Response.json(data);
-}

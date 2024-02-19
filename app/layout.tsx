45dc75d11dde58ea8e2fe7ef45a708452cc23fb7
Josh Kolenko <jkolenko+github@proton.me>
2024-02-18 21:36:04 -0500

    feat: add home link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next';
+
 import { Inter } from 'next/font/google';
+
+import Link from 'next/link';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-gray-900">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + ' p-4'}>
+        <Link href="/" className="btn btn-ghost btn-secondary">
+          Home
+        </Link>
+        {children}
+      </body>
     </html>
   );
 }

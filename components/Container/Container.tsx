62fc7247e9140f79a3945f2c2f578225b9f9f66d
Josh Kolenko <jkolenko+github@proton.me>
2024-02-20 00:19:37 -0500

    feat: update Container default styles

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,9 +1,13 @@
 export default function Container({
   children,
+  className,
 }: {
   children?: React.ReactNode;
+  className?: React.HtmlHTMLAttributes<HTMLDivElement>['className'];
 }) {
   return (
-    <div className="container mx-auto text-center py-16 prose">{children}</div>
+    <div className={'container mx-auto px-6 w-full ' + className}>
+      {children}
+    </div>
   );
 }

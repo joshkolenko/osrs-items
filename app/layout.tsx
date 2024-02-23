bae1ae2eb3cc2f121e5b4ec67e7c749aa16eb1bd
Josh Kolenko <jkolenko+github@proton.me>
2024-02-23 01:54:39 -0500

    feat: add Items context to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link';
 
 import './globals.css';
 import Nav from '@/components/Nav';
+import { ItemsProvider } from '@/context/Items';
+import { getItems } from '@/util/osrs-items';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,16 +16,20 @@ export const metadata: Metadata = {
   description: 'Old School RuneScape Items',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const items = await getItems();
+
   return (
     <html lang="en" data-theme="dracula">
       <body className={inter.className}>
-        <Nav />
-        {children}
+        <ItemsProvider items={items}>
+          <Nav />
+          {children}
+        </ItemsProvider>
       </body>
     </html>
   );
